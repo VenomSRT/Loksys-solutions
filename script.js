f36c5e5073e51fc611f584ff01703c40db40177c
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-12-11 16:45:51 +0200

    desktop styles in process

--- script.js
+++ script.js
@@ -6,15 +6,26 @@ const technologySwiper = new Swiper('.technology__swiper', {
 });
 
 const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.navigation');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if(!menuOpen) {
         menuBtn.classList.add('open');
+        menu.style.display = 'flex';
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
+        menu.style.display = 'none';
         menuOpen = false;
     }
 });
 
 const menuBtnBurger = document.querySelector('.menu-btn__burger');
+
+document.body.addEventListener('resize', () => {
+    if (getComputedStyle(document.body).width >= 1024) {
+        menu.style.display = 'flex';
+    } else {
+        menu.style.display = 'none';
+    }
+})
