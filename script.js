bbf41485ac2cdc9b699d0da376124b1991e90575
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-12-14 00:31:00 +0200

    styling

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const technologySwiper = new Swiper('.technology__swiper', {
         el: '.swiper-pagination',
     },
     slidesPerView: 1,
+
+    breakpoints: {    
+        1356: {
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+        },
+    }
 });
 
 const menuBtn = document.querySelector('.menu-btn');
@@ -10,17 +19,13 @@ const menu = document.querySelector('.navigation');
 let menuOpen = false;
 menuBtn.addEventListener('click', () => {
     if(!menuOpen) {
-        menuBtn.classList.add('open');
         menu.style.display = 'flex';
         menuOpen = true;
     } else {
-        menuBtn.classList.remove('open');
         menu.style.display = 'none';
         menuOpen = false;
     }
 });
-
-const menuBtnBurger = document.querySelector('.menu-btn__burger');
 
 document.body.addEventListener('resize', () => {
     if (getComputedStyle(document.body).width >= 1024) {
